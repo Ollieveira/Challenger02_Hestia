ad01de74384225585858b0c4360a04a97648d682
Ollieveira1 <willysaaoliveira@gmail.com>
2024-04-30 15:44:40 -0300

    Initial commit

---
+++ Challenger02_Hestia/Challenger02_HestiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenger02_HestiaApp.swift
+//  Challenger02_Hestia
+//
+//  Created by Willys Oliveira on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Challenger02_HestiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
