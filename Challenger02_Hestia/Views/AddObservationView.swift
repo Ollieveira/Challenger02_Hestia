459be2e830ad95af63fb646dab11444d69b043e8
Ollieveira1 <willysaaoliveira@gmail.com>
2024-05-23 09:49:00 -0300

    - Ajustes no layout da tela de adiçao de notas.

--- Challenger02_Hestia/Views/AddObservationView.swift
+++ Challenger02_Hestia/Views/AddObservationView.swift
@@ -1,53 +1,64 @@
 import SwiftUI
 
 struct AddObservationView: View {
+    @FocusState private var isTextEditorFocused: Bool
+    
     @Binding var isPresented: Bool
     @State var note: String = ""
     var meal: Meal
     var viewModel: MealViewModel
-
+    
     
     var body: some View {
         VStack {
-            Text("Your notes!")
-                .font(.title)
-                .fontDesign(.rounded)
-                .fontWeight(.bold)
-                .frame(maxWidth: .infinity, alignment: .leading)
-            
-            ZStack(alignment: .topLeading) {
-                TextEditor(text: $note)
-                    .frame(width: 320, height: 192)
-                    .background(Color(UIColor.secondarySystemBackground))
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .padding(5)
-                if note.isEmpty {
-                    Text("Write your notes here...")
-                        .foregroundColor(.gray)
-                        .padding(.top, 14)
-                        .padding(.leading, 12)
-                }
-
-            }
-            .font(.system(.body, design: .rounded))
-
-            Button(action: {
-                viewModel.saveMealNotes(meal, notes: note)
-                isPresented = false
-
-            }, label: {
-                Text("Done")
-                    .font(.title3)
+            VStack {
+                Text("Your notes!")
+                    .font(.title)
                     .fontDesign(.rounded)
-                    .foregroundStyle(Color.white)
-                    .frame(width: 100, height: 50)
-            })
-            .background(Color.tabViewCor)
-            .clipShape(RoundedRectangle(cornerRadius: 12))
-            .frame(maxWidth: .infinity, alignment: .trailing)
-           
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                ZStack(alignment: .topLeading) {
+                    TextEditor(text: $note)
+                        .focused($isTextEditorFocused)
+                        .frame(width: 320, height: 192)
+                        .background(Color(UIColor.secondarySystemBackground))
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .padding(5)
+                    if note.isEmpty {
+                        Text("Write your notes here...")
+                            .foregroundColor(.gray)
+                            .padding(.top, 14)
+                            .padding(.leading, 12)
+                    }
+                    
+                }
+                .font(.system(.body, design: .rounded))
+                
+                Button(action: {
+                    viewModel.saveMealNotes(meal, notes: note)
+                    isPresented = false
+                    
+                }, label: {
+                    Text("Done")
+                        .font(.title3)
+                        .fontDesign(.rounded)
+                        .foregroundStyle(Color.white)
+                        .frame(width: 100, height: 50)
+                })
+                .background(Color.tabViewCor)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                
+            }
+            
+            Spacer()
         }
-        .padding(.horizontal, 36)
+        .contentShape(Rectangle()) // Ensure the entire VStack can receive tap events
+        .onTapGesture {
+            isTextEditorFocused = false
+        }
+        .padding(.horizontal, 36).padding(.vertical, 48)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(RoundedRectangle(cornerRadius: 44)
             .fill(Color.bgFavCardCor)
