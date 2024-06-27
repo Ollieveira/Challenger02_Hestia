ecf497f420ab1b81133b38bbf74315eed680bfb6
Ollieveira1 <willysaaoliveira@gmail.com>
2024-06-26 22:09:40 -0300

    - Ajuste na tela de launch screen para que nao funcione como uma Navigation

--- Challenger02_Hestia/Views/LaunchScreenView.swift
+++ Challenger02_Hestia/Views/LaunchScreenView.swift
@@ -2,74 +2,67 @@ import SwiftUI
 
 struct LaunchScreenView: View {
     @State var viewModel = MealViewModel.instance
-    @State var contador = 1
-    @State var goToNextView = false
+    @State var showLaunchScreen = true
     @State var animaHestia = false
     @State var animaTitle = false
     
-    
-    
     var body: some View {
-        NavigationStack {
-            VStack {
-                
-                if animaHestia {
-                    Spacer()
-                    Spacer()
-                }
-                
-                Spacer()
-                
-                Image("HestiaTitulo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: animaTitle ? 68 : 0)
-                    .animation(.smooth(duration: 0.5), value: animaTitle)
-
-                
-                if !animaTitle {
-                    Spacer()
-                }
-                
-                Spacer()
-                Image("HestiaLaunchScreen")
-                    .resizable()
-                    .scaledToFit()
-                    .offset(y: 50)
-                    .frame(height: animaHestia ? 414 : 0)
-                    .animation(.smooth(duration: 0.5), value: animaHestia)
-
-            }
-            .navigationDestination(isPresented: $goToNextView, destination: {
-                TheTabView()
-            })
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(
-                LinearGradient(gradient: Gradient(colors: [Color.degradeCor, Color.tabViewCor]), startPoint: .bottom, endPoint: .top)
-            )
-            .edgesIgnoringSafeArea(.all)
-            
-            .animation(.default, value: goToNextView)
-            .onAppear{
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                    animaTitle.toggle()
+        Group {
+            if showLaunchScreen {
+                VStack {
+                    if animaHestia {
+                        Spacer()
+                        Spacer()
+                    }
                     
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 1)
-                    {
-                        animaHestia.toggle()
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5)
-                        {
-                            goToNextView.toggle()
+                    Spacer()
+                    
+                    Image("HestiaTitulo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: animaTitle ? 68 : 0)
+                        .animation(.smooth(duration: 0.8), value: animaTitle)
+
+                    
+                    if !animaTitle {
+                        Spacer()
+                    }
+                    
+                    Spacer()
+                    
+                    Image("HestiaLaunchScreen")
+                        .resizable()
+                        .scaledToFit()
+                        .offset(y: 50)
+                        .frame(height: animaHestia ? 414 : 0)
+                        .animation(.smooth(duration: 1), value: animaHestia)
+
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(
+                    LinearGradient(gradient: Gradient(colors: [Color.degradeCor, Color.tabViewCor]), startPoint: .bottom, endPoint: .top)
+                )
+                .edgesIgnoringSafeArea(.all)
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        animaTitle.toggle()
+                        
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            animaHestia.toggle()
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                                showLaunchScreen = false
+                            }
+                        }
+                    }
+                    if viewModel.meals.isEmpty {
+                        Task {
+                            viewModel.loadAllMeals()
                         }
                     }
                 }
-                if viewModel.meals.isEmpty {
-                    Task {
-                        viewModel.loadAllMeals()
-                    }
-                }
+            } else {
+                TheTabView()
             }
-            .animation(.smooth(duration: 0.5), value: animaHestia)
         }
     }
 }
